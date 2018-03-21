73e7b1dca29e8275d1f6d44883aa19c5a0e65d2a
justinunited <71zf@99e>
2018-03-21 23:13:46 +0700

    run web

--- bam.py
+++ bam.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, redirect, request, url_for, flash
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host = 'localhost', port = 5000)
+
+app = Flask(__name__)
+
+
+
 @app.route("/")
 @app.route("/login", methods = ['GET','POST'])
 def login():
     return render_template('include.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = 'localhost', port = 5000)
